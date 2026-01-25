5bcc7ab8fa4a18915f799df42a1ccf8700578cee
Gagandeep Singh <gagandeep.12322960@lpu.in>
2026-01-25 19:10:18 +0530

    loops in cpp

---
+++ loops.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+
+    int i=0;
+    while(i<=n)
+    {
+        int j=0;
+        while(j<i){
+            cout<<"* ";
+            j++;
+        }
+        i++;
+        cout<<endl;
+    }
+
+    do{
+        cout<<n;
+        n--;
+    }while(n!=0);
+
+    
+
+}
